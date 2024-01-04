96c7f57f16790377378522ab5d17561231ea456e
paulgibert <paulgibert98@gmail.com>
2024-01-03 19:18:26 -0500

    Added session unit tests

--- tests/api/unittests/test_workspace.py
+++ tests/api/unittests/test_workspace.py
@@ -7,6 +7,7 @@ import pytest
 
 # Local
 from chaingpt.api import workspace
+from tests.api.unittests.utils import setup_grype_workspace, cleanup_leftover_workspaces
 
 
 def test___random_parent_dir__prefix_provided():
@@ -73,40 +74,6 @@ def test___repo_name__with_suffix():
     """
     repo = workspace._repo_name("https://github.com/user/project.git")
     assert repo == "project"
-
-
-@pytest.fixture
-def setup_grype_workspace(monkeypatch):
-    """
-    Fixture that sets up a workspace containing the grype
-    repository. Copies a large and small test file into the
-    top-level directory.
-    """
-    wk = workspace.Workspace("https://github.com/anchore/grype.git")
-    
-    big_path = os.path.join(wk.repo_dir, "big.txt")
-    with open(big_path, "w") as f:
-        f.write("A"*30000)
-    
-    small_path = os.path.join(wk.repo_dir, "small.txt")
-    with open(small_path, "w") as f:
-        f.write("B"*100)
-    
-    yield wk
-    shutil.rmtree(wk.parent_dir)
-
-
-@pytest.fixture
-def cleanup_leftover_workspaces():
-    """
-    Used in test cases that should fail to create a workspace. Cleans up workspace
-    """
-    yield
-    dirs = os.listdir("/tmp")
-    for d in dirs:
-        if d.startswith("chaingpt"):
-            path = os.path.join("/tmp", d)
-            shutil.rmtree(path)
 
 
 class TestWorkspace:
